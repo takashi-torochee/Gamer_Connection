cebe8a524dd447ea28c6f36876f4093f9c5b404a
EC2 Default User <ec2-user@ip-172-31-2-68.ec2.internal>
2022-03-24 15:45:55 +0000

    [Add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'relationships/follows'
+    get 'relationships/followers'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/withdraw_confirm'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/show'
+  end
+  namespace :public do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
